fe074b15e3e0d516ddf5d174e580d63009f113d3
Skely157 <131863047+Skely157@users.noreply.github.com>
2024-01-17 14:39:57 +0100

    Basic layout and tests

---
+++ bots/music-bot-discord.py
@@ -0,0 +1,9 @@
+
+#link til tutorial video
+#https://www.youtube.com/watch?v=dRHUW_KnHLs&list=LL&index=4
+
+a="Hello world"
+
+
+
+
---
+++ webdesign/my-app/src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<h1>Welcome to JR-Bots</h1>
+
+<p id="test">Julian Tester 123</p>
+
+<script>
+    let counter = 0
+    function increaseCounter(){
+        counter += 1
+    }
+</script>
+
+<button on:click={increaseCounter}>Du har klikket {counter} ganger :) </button>
+
+<style>
+    #test {
+        color: red;
+    }
+
+    :global(body) {
+        background-color: gray;
+    
+    }
+
+</style>
