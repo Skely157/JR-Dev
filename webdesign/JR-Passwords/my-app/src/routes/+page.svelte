2938aaf5c0ccd04247ca655f48bd5cdc89d9c2fb
Skely157 <131863047+Skely157@users.noreply.github.com>
2024-02-14 10:59:27 +0100

    Scratch website

---
+++ webdesign/JR-Passwords/my-app/src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<body>
+    <div id="login-container">
+        <h2>Login</h2>
+        <form action=" " method="POST">
+            <div class="login-group">
+                <label for="username">Username:</label><br>
+                <input type="text" id="username" name="username" required><br>
+            </div>
+            <div class="login-group">
+                <label for="password">Password:</label><br>
+                <input type="password" id="password" name="password"><br><br>
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+    </div>
+</body>
+
+
